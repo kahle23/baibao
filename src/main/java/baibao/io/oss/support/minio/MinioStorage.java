81e736a34883564eacbb2724120b854ede2aaddf
Kahle <kahle@undefined>
2023-07-18 22:40:03 +0800

    [feat] 增加 MinIO 存储工具实现类

---
+++ src/main/java/baibao/io/oss/support/minio/MinioStorage.java
@@ -0,0 +1,145 @@
+package baibao.io.oss.support.minio;
+
+import artoria.exception.ExceptionUtils;
+import artoria.io.oss.OssBase;
+import artoria.io.oss.OssInfo;
+import artoria.io.oss.OssObject;
+import artoria.io.oss.support.AbstractOssStorage;
+import artoria.io.oss.support.OssObjectImpl;
+import artoria.util.Assert;
+import artoria.util.CloseUtils;
+import io.minio.*;
+import io.minio.errors.ErrorResponseException;
+import io.minio.messages.ErrorResponse;
+import io.minio.messages.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class MinioStorage extends AbstractOssStorage {
+    private static final Logger log = LoggerFactory.getLogger(MinioStorage.class);
+    private final MinioClient minioClient;
+    private final Long partSize;
+
+    public MinioStorage(MinioClient minioClient, Map<String, String> objectUrlPrefixes,
+                        Long partSize, String defaultBucket) {
+        super(objectUrlPrefixes, defaultBucket);
+        Assert.notNull(minioClient, "Parameter \"minioClient\" must not null. ");
+        if (partSize == null || partSize <= 0) { partSize = 5L * 1024 * 1024; }
+        this.minioClient = minioClient;
+        this.partSize = partSize;
+    }
+
+    public MinioStorage(MinioClient minioClient, Map<String, String> objectUrlPrefixes,
+                        Long partSize) {
+
+        this(minioClient, objectUrlPrefixes, partSize, null);
+    }
+
+    public MinioStorage(MinioClient minioClient, Map<String, String> objectUrlPrefixes) {
+
+        this(minioClient, objectUrlPrefixes, null, null);
+    }
+
+    @Override
+    public MinioClient getNative() {
+
+        return minioClient;
+    }
+
+    @Override
+    public boolean exist(Object key) {
+        OssBase ossBase = getOssBase(key);
+        String bucketName = ossBase.getBucketName();
+        String objectKey = ossBase.getObjectKey();
+        try {
+            minioClient.statObject(StatObjectArgs.builder()
+                    .bucket(bucketName).object(objectKey).build());
+            return true;
+        }
+        catch (ErrorResponseException e) {
+            ErrorResponse errorResponse = e.errorResponse();
+            String code = errorResponse.code();
+            if ("NoSuchKey".equals(code)) { return false; }
+            else { throw ExceptionUtils.wrap(e); }
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+    }
+
+    @Override
+    public OssObject get(Object key) {
+        OssBase ossBase = getOssBase(key);
+        String bucketName = ossBase.getBucketName();
+        String objectKey = ossBase.getObjectKey();
+        GetObjectResponse objectResponse;
+        try {
+            objectResponse = minioClient.getObject(GetObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(objectKey)
+                    .build());
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+        if (objectResponse == null) { return null; }
+        OssObjectImpl ossObject = new OssObjectImpl();
+        ossObject.setBucketName(objectResponse.bucket());
+        ossObject.setObjectKey(objectResponse.object());
+        ossObject.setMetadata(objectResponse.headers());
+        ossObject.setObjectContent(objectResponse);
+        return ossObject;
+    }
+
+    @Override
+    public OssInfo put(Object data) {
+        OssObject ossObject = convertToOssObject(data);
+        InputStream inputStream = null;
+        try {
+            inputStream = ossObject.getObjectContent();
+            ObjectWriteResponse objectWriteResponse = minioClient.putObject(PutObjectArgs.builder()
+                    .bucket(ossObject.getBucketName())
+                    .object(ossObject.getObjectKey())
+                    .stream(inputStream, -1, partSize)
+                    .build());
+            String bucketName = objectWriteResponse.bucket();
+            String objectKey = objectWriteResponse.object();
+            return buildOssInfo(bucketName, objectKey, null, objectWriteResponse);
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+        finally {
+            CloseUtils.closeQuietly(inputStream);
+        }
+    }
+
+    @Override
+    public Object delete(Object key) {
+        try {
+            OssBase ossBase = getOssBase(key);
+            String bucketName = ossBase.getBucketName();
+            String objectKey = ossBase.getObjectKey();
+            minioClient.removeObject(RemoveObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(objectKey)
+                    .build());
+            return null;
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+    }
+
+    @Override
+    public Object list(Object conditions) {
+        // todo minioClient keys
+        ListObjectsArgs args = null;
+        Iterable<Result<Item>> results = minioClient.listObjects(args);
+        return results;
+    }
+
+}
