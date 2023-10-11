e027bab0172a078426fd11bdf8a853c491d5dd2e
Kahle <kahle@undefined>
2023-10-12 01:03:41 +0800

    [feat] 增加老代码“根据设备型号查询设备信息”、“发送钉钉群机器人消息”和“发送企微群机器人消息”

---
+++ src/test/java/baibao/extension/device/support/FileBasedDeviceQueryHandlerTest.java
@@ -0,0 +1,33 @@
+package baibao.extension.device.support;
+
+import artoria.action.ActionUtils;
+import artoria.exception.ExceptionUtils;
+import baibao.extension.device.Device;
+import baibao.extension.device.DeviceQuery;
+import com.alibaba.fastjson.JSON;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.Boolean.TRUE;
+
+@Ignore
+public class FileBasedDeviceQueryHandlerTest {
+    private static Logger log = LoggerFactory.getLogger(FileBasedDeviceQueryHandlerTest.class);
+
+    @Test
+    public void test1() {
+        try {
+            new DeviceAutoConfiguration().afterPropertiesSet();
+
+            DeviceQuery deviceQuery = new DeviceQuery("SM901");
+            Device device = ActionUtils.execute(deviceQuery, Device.class);
+            log.info(JSON.toJSONString(device, TRUE));
+        }
+        catch (Exception e) {
+            throw ExceptionUtils.wrap(e);
+        }
+    }
+
+}
