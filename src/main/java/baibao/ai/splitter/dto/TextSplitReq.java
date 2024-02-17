05549a5ae1040d6aa03630074ddd8c810ecb002f
Kahle <kahle@undefined>
2024-02-17 22:44:29 +0800

    [opti] 增加 Copyright 信息

--- src/main/java/baibao/ai/splitter/dto/TextSplitReq.java
+++ src/main/java/baibao/ai/splitter/dto/TextSplitReq.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019. the original author or authors.
+ * BaiBao is licensed under the "LICENSE" file in the project's root directory.
+ */
+
 package baibao.ai.splitter.dto;
 
 import java.io.Serializable;
--- src/main/java/baibao/extension/company/Company.java
+++ src/main/java/baibao/extension/company/Company.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019. the original author or authors.
+ * BaiBao is licensed under the "LICENSE" file in the project's root directory.
+ */
+
 package baibao.extension.company;
 
 import java.io.Serializable;
